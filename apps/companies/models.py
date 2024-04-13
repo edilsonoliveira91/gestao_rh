c37787edaee51aa8f63b999c3ec9157078341561
Edilson Oliveira <edilsonoliveira@me.com>
2024-04-12 23:23:26 -0300

    Create app companies

---
+++ apps/companies/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Companies(models.Model):
+    name = models.CharField(max_length=100, help_text='Nome da empresa')
+
+    def __str__(self) -> str:
+        return self.name
+
